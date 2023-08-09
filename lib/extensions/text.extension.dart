080ea30c2dfc20a42189bd65d987ca6c0be40124
WangJi <jixwang@thoughtworks.com>
2023-08-09 10:01:44 +0800

    rename file and add some text extensions

---
+++ lib/extensions/text.extension.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+extension TextWidgetExtension on Text {
+  Text color(Color color) {
+    var existedStyle = style ?? const TextStyle();
+    return Text(
+      data ?? "",
+      style: existedStyle.copyWith(color: color),
+    );
+  }
+
+  Text fontSize(double size) {
+    var existedStyle = style ?? const TextStyle();
+    return Text(
+      data ?? "",
+      style: existedStyle.copyWith(fontSize: size),
+    );
+  }
+
+  Text underline() {
+    var existedStyle = style ?? const TextStyle();
+    return Text(
+      data ?? "",
+      style: existedStyle.copyWith(decoration: TextDecoration.underline),
+    );
+  }
+}
