9997aa2d0caa5d59ff70a8ef011dcd4a99214407
imwangji <imwangji@qq.com>
2023-08-12 18:17:49 +0800

    update input_item

--- lib/components/input_item/ex_input_item.dart
+++ lib/components/input_item/ex_input_item.dart
@@ -1,3 +1,5 @@
+import 'dart:js_interop';
+
 import 'package:ex_widget/extensions/padding.extension.dart';
 import 'package:ex_widget/extensions/text.extension.dart';
 import 'package:flutter/material.dart';
@@ -5,7 +7,13 @@ import 'package:flutter/material.dart';
 class EXInputItem extends StatefulWidget {
   final String label;
   final String? hintText;
-  const EXInputItem({super.key, required this.label, this.hintText});
+  final Widget? prefix;
+  const EXInputItem({
+    super.key,
+    required this.label,
+    this.hintText,
+    this.prefix,
+  });
 
   @override
   State<EXInputItem> createState() => _EXInputItemState();
@@ -18,18 +26,23 @@ class _EXInputItemState extends State<EXInputItem> {
       color: Colors.white,
       child: Flex(
         direction: Axis.horizontal,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          widget.prefix.isNull ? Container() : widget.prefix!.pr(32),
           Text(widget.label).fontSize(32).pr(32),
           Flexible(
             child: TextField(
-              style: TextStyle(
+              minLines: 1,
+              maxLines: null,
+              style: const TextStyle(
                 fontSize: 32,
                 color: Colors.black,
               ),
               decoration: InputDecoration(
+                contentPadding: EdgeInsets.zero,
                 border: InputBorder.none,
                 hintText: widget.hintText,
-                hintStyle: TextStyle(
+                hintStyle: const TextStyle(
                   color: Color(0xffb3b3b3),
                   fontSize: 32,
                 ),
